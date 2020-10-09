88a967313bbf53d5a539587eb0c7997502ac469b
Jakob Samonig <samonij@wellingtoncollege.org.uk>
2020-10-09 17:53:53 +0100

    Added logging

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,9 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_fontawesome import FontAwesome
 from flask_moment import Moment
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 #  --> Adapted from https://blog.miguelgrinberg.com/
 app = Flask(__name__)
@@ -20,5 +23,19 @@ fa = FontAwesome(app)
 moment = Moment(app)
 
 from app import routes, models, errors
+
+
+if not app.debug:
+    # ...
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('App startup')
 
 # <--
